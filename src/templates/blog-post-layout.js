395bf8f4c259edb7ca7fed59492f5d67036fb333
ruucm <ruucm.a@gmail.com>
2020-01-25 20:32:50 +0900

    rm unusings

--- src/templates/blog-post-layout.js
+++ src/templates/blog-post-layout.js
@@ -11,6 +11,7 @@ const Wrap = styled.div`
   padding-top: 134px;
   width: 100%;
 `
+const Title = styled.h1``
 
 export default function PageTemplate({ data: { current, prev, next } }) {
   const intl = useIntl()
@@ -21,91 +22,9 @@ export default function PageTemplate({ data: { current, prev, next } }) {
     <Layout>
       <SEO title={current.frontmatter.title} />
       <Wrap>
-        <ProductCover>
-          <CoverRow>
-            <ProductNode align="left">
-              <Summary
-                category={current.frontmatter.category}
-                title={current.frontmatter.title}
-                desc={current.frontmatter.excerpt}
-                float="left"
-              />
-            </ProductNode>
-          </CoverRow>
-          <TitleCover>
-            <CoverImage
-              img={featuredImgFluid}
-              mobileImg={featuredImgFluid}
-              height="450"
-            />
-          </TitleCover>
-          <CoverFooter>
-            <FooterBorder />
-            <System.Text
-              type="Body-Short-1"
-              text="Need Framer X to learn this class"
-            />
-          </CoverFooter>
-        </ProductCover>
-
-        <ProductBody>
-          <ProductWrap>
-            <ProductArticle>
-              <MDXRenderer>{current.body}</MDXRenderer>
-            </ProductArticle>
-          </ProductWrap>
-        </ProductBody>
+        <Title>{current.frontmatter.title}</Title>
+        <MDXRenderer>{current.body}</MDXRenderer>
       </Wrap>
-
-      <StartCouseWrap>
-        <BannerWrap>
-          <TitleWrap>
-            <System.Text
-              type="Heading-Short-2"
-              align="center"
-              text="Clone Apple Music with Framer X"
-            />
-          </TitleWrap>
-          <DesscWrap>
-            <System.Text
-              type="Body-Short-2"
-              align="center"
-              text="Learn Framer X start from scratch to making whole Apple Music as same"
-            />
-          </DesscWrap>
-          {current.frontmatter.order === 0 ? (
-            userInput.member ? (
-              <Link to={next.fields.slug}>
-                <System.Button
-                  width="130"
-                  height="35"
-                  type="primary-1"
-                  text="Start Course"
-                />
-              </Link>
-            ) : (
-              <System.Button
-                onClick={() => {
-                  setModal({ showModal: true, serviceModalType: "Member" })
-                }}
-                width="130"
-                height="35"
-                type="primary-1"
-                text="Start Course"
-              />
-            )
-          ) : (
-            <PostLinks>
-              {userInput.member && prev && (
-                <PostLink to={prev.fields.slug}>Prev</PostLink>
-              )}
-              {userInput.member && next && (
-                <PostLink to={next.fields.slug}>Next</PostLink>
-              )}
-            </PostLinks>
-          )}
-        </BannerWrap>
-      </StartCouseWrap>
     </Layout>
   )
 }
